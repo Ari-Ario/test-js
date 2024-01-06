232f1f9c630988ad70dd2ccbb8999e98f20f7c38
Ari-Ario <xusrew.z@gmail.com>
2024-01-06 10:00:00 +0100

    code of ROT-encoding finished

--- advanced/Conditional_Iterative/ROT_Encoding/rot.php
+++ advanced/Conditional_Iterative/ROT_Encoding/rot.php
@@ -1,3 +1,31 @@
 <?php
+function rot13($passcode, $n){
+    $passArray= str_split($passcode);
+    $str = "";
+    foreach ($passArray as $char){
+        $assciValue = ord($char);
+        if ($assciValue>= 65 && $assciValue<= 90){
+            $assciValue += $n;
+            if ($assciValue > 90) {
+                $encodedValue = 65 + ($assciValue%90) -1;
+            } else {
+                $encodedValue = $assciValue;
+            }
+        } elseif ($assciValue>= 97 && $assciValue<= 122){
+            $assciValue += $n;
+            if ($assciValue > 122){
+                $encodedValue = 97 + ($assciValue%122) -1;
+            } else {
+                $encodedValue = $assciValue;
+            }
+        } else {
+            $encodedValue = $assciValue;
+        }
+        $encodedChar = chr($encodedValue);
+        $str .= $encodedChar;
+    }
 
+    return $str;
+}
+print(rot13("HELLO, hello", 13))
 ?>
