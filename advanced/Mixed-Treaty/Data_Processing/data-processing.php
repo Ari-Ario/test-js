a78fa240c2b82ab0a0edb42664152a4014daa8a1
Ari-Ario <xusrew.z@gmail.com>
2024-01-07 15:22:03 +0100

    php file and basic code for data-processing

---
+++ advanced/Mixed-Treaty/Data_Processing/data-processing.php
@@ -0,0 +1,10 @@
+<?php
+
+function processData($path){
+    echo "sth";
+}
+
+// write or copy-paste the path below
+$path = "/Users/ari/Documents/gitfolder/test-js/advanced/Mixed-Treaty/Data_Processing/test.txt";
+processData($path);
+?>
